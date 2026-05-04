a3ae5be01012da7fc83482f2ee511bc52653a564
anita <nageroa6@gmail.com>
2026-05-04 21:57:06 +0200

    Modificato in file main.js. Terminato.

--- main.js
+++ main.js
@@ -20,10 +20,63 @@
 // Input: “i topi non avevano nipoti”
 // Output: true
 
-function isPalindrom(str){
+
+function isPalindroma(str) {
+
+let strPulita = str.replace(/\W/g, "").toLowerCase();
 
 
-    //rimuove spazi e segni di P.
-
-    const cleanedStr =  str.replace(/\W/g, "")
+//creo, inversione str
+let invertita= "";
+for ( let i = strPulita.length -1; i >= 0; i--) {
+    invertita = invertita + strPulita[i];
 }
+//pongo confronto a resa di pulizia stringa e codice - funzione - scrittura e lettura; per valore boolean
+if (strPulita === invertita) {
+    return true;
+}else {
+    return false;
+}
+
+}
+
+
+
+//ORA LE MIE FRASI DI CONSOLE.
+
+let input= "i topi non avevano nipoti";
+
+console.log(input);
+
+//
+
+console.log(isPalindroma(input));
+
+
+
+   
+// stringa1 = stringa1.toLowerCase().replace(/\W/g, "");
+// stringa2 = stringa2.toLowerCase().replace(/\W/g, "");
+
+
+// return stringa1 === stringa2;
+
+//     };
+
+//     console.log(puliscilaUnPò(stringa1,stringa2));
+//out put : 
+
+
+
+// function confrontaMi(str1,str2){
+//     str1= str1.toLowerCase().replace(/\W/g, "");
+//     str2=str2.toLowerCase().replace(/\W/g, "");
+
+//     return str1 === str2;
+// }
+
+
+// let caotIci = "???i topi non avevano ni poti!!!"
+// let pilitIssimo = "i topi non avevano nipoti";
+
+// console.log(confrontaMi(caotIci, pilitIssimo));
